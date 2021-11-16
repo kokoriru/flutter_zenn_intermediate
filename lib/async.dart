6e6c1e9c1ea7ba2bea8a906f471a80aeea686985
kokoriru <yuka@ryukalice.com>
2021-11-16 11:53:36 +0900

    動的取得値の値を表示するタイミングについて await を用いて、待ち合わせを行いその場で非同期関数の終了を待つ形で設定

--- lib/async.dart
+++ lib/async.dart
@@ -2,27 +2,18 @@ import 'dart:async';
 import 'dart:io';
 
 class Async {
-  void asynctest3() async {
+  void asynctest4() async {
     print("method begin");
     print(DateTime.now().toString());
 
     print('data1 start');
-    Future<String> result1 = asyncFunc('data1', 3);
-    result1.then((result) {
-      print(result);
-    });
+    print(await asyncFunc('data1', 3));
 
     print('data2 start');
-    Future<String> result2 = asyncFunc('data2', 2);
-    result2.then((result) {
-      print(result);
-    });
+    print(await asyncFunc('data2', 2));
 
     print('data3 start');
-    Future<String> result3 = asyncFunc('data3', 1);
-    result3.then((result) {
-      print(result);
-    });
+    print(await asyncFunc('data3', 1));
   }
 
   // time の時間分スリープし、その後、現在時間を返す関数

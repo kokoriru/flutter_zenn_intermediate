4608c10ff93eb393e741f1e271a94be0f4df0192
kokoriru <yuka@ryukalice.com>
2021-11-16 11:46:56 +0900

    非同期の動き確認として、ボタン押下で設定時間遅延してターミナルに戻り値を表示する処理に変更(動的取得値は Future<String> になる)

--- lib/async.dart
+++ lib/async.dart
@@ -2,20 +2,21 @@ import 'dart:async';
 import 'dart:io';
 
 class Async {
-  void asynctest1() {
+  void asynctest2() {
     print('method begin');
     print(DateTime.now().toString());
     print('data1 start');
-    print(syncFunc('data1', 3));
+    print(asyncFunc('data1', 3));
     print('data2 start');
-    print(syncFunc('data2', 2));
+    print(asyncFunc('data2', 2));
     print('data3 start');
-    print(syncFunc('data3', 1));
+    print(asyncFunc('data3', 1));
   }
 
   // time の時間分スリープし、その後、現在時間を返す関数
-  String syncFunc(String name, int time) {
-    sleep(Duration(seconds: time));
-    return name + ':' + DateTime.now().toString();
+  Future<String> asyncFunc(String name, int time) async {
+    return Future.delayed(Duration(seconds: time), () {
+      return name + ':' + DateTime.now().toString();
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
-    Async().asynctest1();
+    Async().asynctest2();
   }
 
   @override

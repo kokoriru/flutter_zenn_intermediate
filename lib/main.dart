90fc4d06b4660b667fa68a1029f3de7107040a67
kokoriru <yuka@ryukalice.com>
2021-11-17 16:22:14 +0900

    import する言語設定のファイル名を修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'l10n/l10n.dart';
 import 'dart:async';
 
 import 'business_logic.dart';
